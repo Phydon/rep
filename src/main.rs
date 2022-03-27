bfd352a7eeca88a5bb872ad4f5bf1b8f7148bf5d
Phydon <leann.phydon@gmail.com>
2022-03-27 13:41:44 +0200

    first stuff written

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::process::Command;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut env_args = Vec::new();
+    // TODO write env_args/command to file -> replace this and then read from file
+    let mut cmd_container = Vec::new();
+
+    for arg in env::args().skip(1) {
+        if arg == String::from("rep") {
+            env_args.push(arg);
+        } else {
+            let arg_cont = arg.clone();
+            cmd_container.push(arg_cont);
+            env_args.push(arg);
+        }
+    }
+    
+    if env_args.is_empty() {
+        eprintln!("Nothing given");
+        std::process::exit(1);
+    }
+
+    println!("Container: {:?}", cmd_container);
+
+    let cmd = &env_args[1];
+    let params = &env_args[2..];
+    
+    println!("cmd: {:?}", cmd);
+    println!("params: {:?}", params);
+
+    execute_command(&cmd, &params);
+}
+
+fn execute_command(cmd: &String, parameters: &[String]) {
+    if cfg!(target_os = "windows") {
+        todo!()
+        // Command::new("ls").arg("-a").arg("-l").status().unwrap();
+    } else {
+        Command::new(cmd).args(parameters).status().unwrap();
+    }
 }
