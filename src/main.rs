2df1daf834fd4a317d4d39f91249598f8bc763d7
Phydon <leann.phydon@gmail.com>
2022-04-04 21:33:29 +0200

    added cmd log

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::process::Command;
-use std::env;
+use std::{env, fs, io};
+use std::io::Write;
 
 fn main() {
     let mut env_args = Vec::new();
-    // TODO write env_args/command to file -> replace this and then read from file
-    let mut cmd_container = Vec::new();
-
+    
     for arg in env::args().skip(1) {
-        let arg_cont = arg.clone();
-        cmd_container.push(arg_cont);
         env_args.push(arg);
     }
-    
+
     if env_args.is_empty() {
-        eprintln!("USAGE: rep <COMMAND> <FLAGS>");
+        eprintln!("USAGE: rep [COMMAND] <FLAGS>");
         std::process::exit(1);
     }
+
+    log_cmd(&mut env_args).expect("Failed to log command");
 
     let cmd = &env_args[0];
     let parameters = &env_args[1..];
@@ -27,6 +26,7 @@ fn execute_command(cmd: &String, parameters: &[String]) {
     if cfg!(target_os = "windows") {
         todo!()
     } else {
+        // TODO
         // may cause problems with selfmade aliases
         // e.g. error: "unknown command: sf"
         // -> defining aliases as ENV variables could solve this
@@ -39,4 +39,16 @@ fn execute_command(cmd: &String, parameters: &[String]) {
             }
         };
     }
+}
+
+fn log_cmd(cmd_list: &mut Vec<String>) -> io::Result<()> {
+    let file_path = "/home/phydon/main/rep/cmd_log.txt";
+
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_path)?;
+
+    writeln!(file, "{:}", cmd_list.join(" "))?;
+    Ok(())
 }
